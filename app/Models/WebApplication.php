f59108fd6866dc20124ac318c0e48e0df8237a09
John Paul Gulayan <jpvgulayan@dswd.gov.ph>
2023-08-11 13:12:11 +0800

    update image url

--- app/Models/WebApplication.php
+++ app/Models/WebApplication.php
@@ -9,4 +9,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class WebApplication extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'application_name',
+        'application_url',
+        'application_image',
+        'application_description',
+        'keywords',
+    ];
+
+    public function getApplicationImageAttribute($value)
+    {
+        return url($value);
+    }
 }
