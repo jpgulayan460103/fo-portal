1eff2d68004abd2bb22d42066d8db78e43fb6869
John Paul Gulayan <jpvgulayan@dswd.gov.ph>
2023-08-11 14:21:29 +0800

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/', [HomeController::class, 'index'])->name('login');
 
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 Route::post('/login', [LoginController::class, 'login'])->name('auth.login');
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('/register', [RegisterController::class, 'show'])->name('register.index');
 
 Route::get('/auth/google/signin', [LoginController::class, 'signInwithGoogle'])->name('auth.google.signin');
